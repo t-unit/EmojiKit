41f3f0d3e921857ee205cf8bce6d8d5daa7161d9
Tobias Ottenweller <tobi@voiceup.chat>
2020-11-12 16:10:25 +0100

    disable swiftlint (for testing)

--- Scripts/lint.sh
+++ Scripts/lint.sh
@@ -11,5 +11,5 @@ function convertToGitHubActionsLoggingCommands() {
     sed -E 's/^(.*):([0-9]+):([0-9]+): (warning|error|[^:]+): (.*)/::\4 file=\1,line=\2,col=\3::\5/'
 }
 
-set -o pipefail && swiftlint --path Sources --config .swiftlint.yml | stripPWD | convertToGitHubActionsLoggingCommands
+# set -o pipefail && swiftlint --path . --config .swiftlint.yml | stripPWD | convertToGitHubActionsLoggingCommands
 set -o pipefail && swiftformat . --swiftversion 5 --lint 2> >(stripPWD) | convertToGitHubActionsLoggingCommands
