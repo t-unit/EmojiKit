e414dcccb50ffc0e0c4def51688cb21d2aef432e
Tobias Ottenweller <tobi@voiceup.chat>
2020-11-12 16:26:13 +0100

    cleanup

--- Scripts/lint.sh
+++ Scripts/lint.sh
@@ -11,5 +11,5 @@ function convertToGitHubActionsLoggingCommands() {
     sed -E 's/^(.*):([0-9]+):([0-9]+): (warning|error|[^:]+): (.*)/::\4 file=\1,line=\2,col=\3::\5/'
 }
 
-# set -o pipefail && swiftlint --path . --config .swiftlint.yml | stripPWD | convertToGitHubActionsLoggingCommands
-set -o pipefail && swiftformat . --swiftversion 5 --lint 2> >(stripPWD) | convertToGitHubActionsLoggingCommands
+swiftlint --path . --config .swiftlint.yml --quiet --strict | stripPWD | convertToGitHubActionsLoggingCommands
+swiftformat . --swiftversion 5 --lint 2> >(stripPWD) | convertToGitHubActionsLoggingCommands
